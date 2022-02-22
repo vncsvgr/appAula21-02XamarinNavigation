56894d912dc7c3341f06e9597723494e5caba223
Usuario <Usuario@CPD03-M12>
2022-02-21 21:54:22 -0300

    Adicionar arquivos de projeto.

---
+++ appAula21_02/appAula21_02/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appAula21_02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btnProxima_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pagina02());
+        }
+    }
+}
---
+++ appAula21_02/appAula21_02/Pagina02.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appAula21_02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina02 : ContentPage
+    {
+        public Pagina02()
+        {
+            InitializeComponent();
+        }
+
+        async void btnAnterior_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
